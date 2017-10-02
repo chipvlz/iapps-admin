8ce949f4d24444e1d32cde7b464832f7e9ae256f
gebeto <bboyheadman@gmail.com>
2017-10-02 22:57:59 +0300

    modal is working

--- src/ActivationKeysApp/actions.js
+++ src/ActivationKeysApp/actions.js
@@ -1,6 +1,6 @@
 export const loadKeysList = (page) => dispatch => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://asdasd/activation/api/getKeys.php');
+    xhr.open('GET', 'http://i98888jy.bget.ru/activation/api/getKeys.php');
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const json = JSON.parse(xhr.responseText);
@@ -13,17 +13,19 @@ export const loadKeysList = (page) => dispatch => {
 
 
 export const updateKey = (e) => dispatch => {
-    // var formData = new FormData(e.target);
-    // console.log(formData);
-    // xhr.open('POST', 'http://asdasd/activation/api/editKey.php', true);
-    // fetch("http://asdasd/activation/api/editKey.php", {
-    //     headers: {
-    //         'Content-Type': 'application/json; charset=utf-8'
-    //     },
-    //     credentials: 'include',
-    //     method: "POST",
-    //     body: JSON.stringify({a: 10})
-    // })
-    // .then(function(res){ console.log(res) })
-    // .catch(function(res){ console.log(res) })
+    const fd = new FormData();
+    fd.append('price', e.target.querySelector('#price').value);
+    fd.append('cnt', e.target.querySelector('#devices-num').value);
+    fd.append('user_profile', e.target.querySelector('#link').value);
+    fd.append('id', e.target.querySelector('#id').value);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://i98888jy.bget.ru/activation/api/editKey.php', true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            console.log(xhr.responseText);
+            window.jQuery('#keysAppModal').modal('hide');
+        }
+    }
+    xhr.send(fd);
 };
--- src/ActivationKeysApp/components/Modal.js
+++ src/ActivationKeysApp/components/Modal.js
@@ -6,40 +6,51 @@ import {
     updateKey
 } from '../actions.js'
 
-const Modal = ({ data, updateKeyHandler }) => {
-    console.log(data);
-    const key = Date.now();
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.id = null;
+        this.price = null;
+        this.cnt = null;
+        this.user_profile = null;
+    }
 
-    return (
-        <div id="keysAppModal" className="modal fade" role="dialog">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        <h4 className="modal-title">{data && data.id}: {data && data.user_profile}</h4>
+    render() {
+        const key = Date.now();
+        if (this.props.data)
+            return (
+                <div id="keysAppModal" className="modal fade" role="dialog">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                <h4 className="modal-title">{this.props.data.id}: {this.props.data.user_profile}</h4>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={this.props.updateKeyHandler} className="clearfix">
+                                    <input type="hidden" value={this.props.data.id} id="id"/>
+                                    <div className="form-group">
+                                        <label htmlFor="price">Цена:</label>
+                                        <input key={key} defaultValue={this.props.data.price} type="number" id="price" className="form-control" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="devices-num">Количество девайсов:</label>
+                                        <input key={key} defaultValue={this.props.data.cnt} type="number" id="devices-num" className="form-control" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="link">Ссылка на профиль:</label>
+                                        <input key={key} defaultValue={this.props.data.user_profile} type="url" id="link" className="form-control" />
+                                    </div>
+                                    <button type="submit" className="btn btn-success pull-right">Сохранить</button>
+                                    <button type="button" className="btn btn-default" data-dismiss="modal">Закрыть</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div className="modal-body">
-                        <form onSubmit={updateKeyHandler} className="clearfix">
-                            <input type="hidden" id={data && data.id} />
-                            <div className="form-group">
-                                <label htmlFor="price">Цена:</label>
-                                <input key={key} defaultValue={data && data.price} type="number" id="price" className="form-control" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="devices-num">Количество девайсов:</label>
-                                <input key={key} defaultValue={data && data.cnt} type="number" id="devices-num" className="form-control" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="link">Ссылка на профиль:</label>
-                                <input key={key} defaultValue={data && data.user_profile} type="url" id="link" className="form-control" />
-                            </div>
-                            <button type="submit" className="btn btn-success pull-right">Сохранить</button>
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Закрыть</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>);
+                </div>);
+        return <div id="keysAppModal" className="modal fade" role="dialog"></div>;
+    }
 }
 
 export default connect(
